4ba89b3336f201e7d81ef2c40f10d6be031f0052
Paulo JRN <paulo.xcco@gmail.com>
2022-02-11 02:14:05 -0200

    HTTP Verbs

--- api-series/app/Http/Controllers/SerieController.php
+++ api-series/app/Http/Controllers/SerieController.php
@@ -3,11 +3,55 @@
 namespace App\Http\Controllers;
 
 use App\Models\Serie;
+use Illuminate\Http\Request;
 
 class SerieController
 {
     public function index()
     {
         return Serie::all();
+    }
+
+    public function store(Request $request)
+    {
+        $serie = Serie::create($request->all());
+
+        return response()->json($serie, 201);
+    }
+
+    public function show(int $id)
+    {
+        $serie = Serie::find($id);
+
+        if (is_null($serie)) {
+            return response()->json("", 204);
+        }
+
+        return response()->json($serie);
+    }
+
+    public function update(int $id, Request $request)
+    {
+        $serie = Serie::find($id);
+
+        if (is_null($serie)) {
+            return response()->json(["erro" => "Recurso não encontrado"], 404);
+        }
+
+        $serie->fill($request->all());
+        $serie->save();
+
+        return $serie;
+    }
+
+    public function destroy(int $id, Request $request)
+    {
+        $qtdRecursosRemovidas = Serie::destroy($id);
+
+        if ($qtdRecursosRemovidas === 0) {
+            return response()->json(["erro" => "Recurso não encontrado"], 404);
+        }
+
+        return response()->json("", 204);
     }
 }
--- api-series/routes/web.php
+++ api-series/routes/web.php
@@ -2,6 +2,8 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
+use App\Http\Controllers\SerieController;
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -17,7 +19,11 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->group(["prefix" => "/api"], function () use ($router) {
-    $router->get("/series", SerieController::class . "@index");
-    $router->get("/episodios", SerieController::class . "@index");
+$router->group(["prefix" => "api"], function () use ($router) {
+    $router->post("series", "SerieController@store");
+    $router->get("series", "SerieController@index");
+    $router->get("series/{id}", "SerieController@show");
+    $router->put("series/{id}", "SerieController@update");
+    $router->delete("series/{id}", "SerieController@destroy");
+    $router->get("episodios", "SerieController@index");
 });
